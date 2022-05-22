3a66081b1b2630649b2756e0fe31f1403c250545
125007007 <ashezzone@gmail.com>
2022-05-23 00:28:40 +1200

    Updated

--- readDatabase.py
+++ readDatabase.py
@@ -5,15 +5,19 @@ if len(sys.argv) < 2:
     print('Usage: readDatabase.py <database name>')
     sys.exit()
 
-if sys.argv[1] == 'sensor1Data.db':
-    conn = sqlite3.connect('sensor1Data.db')
-elif sys.argv[1] == 'sensor2Data.db':
-    conn=sqlite3.connect('sensor2Data.db')
+if sys.argv[1] == 'sensor1Data.db' or sys.argv[1] == 'sensor2Data.db':
+    conn = sqlite3.connect(sys.argv[1])
+    curs = conn.cursor()
+    print ("\nEntire database contents:\n")
+    for row in curs.execute("SELECT * FROM DHT_data"):
+        print(row)
+elif sys.argv[1] == 'serverCPU.db':
+    conn=sqlite3.connect(sys.argv[1])
+    curs = conn.cursor()
+    print ("\nEntire database contents:\n")
+    for row in curs.execute("SELECT * FROM CPU_temps"):
+        print(row)
 else:
     print('Usage: readDatabase.py <database name>')
     sys.exit()
 
-curs = conn.cursor()
-print ("\nEntire database contents:\n")
-for row in curs.execute("SELECT * FROM DHT_data"):
-    print(row)
